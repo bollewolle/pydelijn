8d51c9f2961a25bc378da02935bedfb090aa7ba5
Helldog136 <130792@student.umons.ac.be>
2020-02-10 18:06:25 +0100

    Worked around stop_ids problems. Will modify it when stib has answered questions

--- pystibmvib/api.py
+++ pystibmvib/api.py
@@ -7,14 +7,14 @@ of STIB/MVIB, the public transportation company of Brussels (Belgium).
 This code is released under the terms of the MIT license. See the LICENSE
 file for more details.
 """
-from datetime import *
-import pytz
 import json
+from datetime import *
+
+import pytz
 
 from .common import BASE_URL, LOGGER
-from .shapefile_reader import ShapefileReader
-
 from .common import CommonFunctions
+from .shapefile_reader import ShapefileReader
 
 PASSING_TIME_BY_POINT_SUFFIX = "/OperationMonitoring/4.0/PassingTimeByPoint/"
 
@@ -34,10 +34,33 @@ def convert_to_utc(localtime, timeformat):
 
 def get_time_delta(t1, t2):
     if not type(t1) == type(datetime.now()):
-        t1 = datetime.strptime('05/01/2012', "%")
+        t1 = datetime.strptime(str(t1), "%Y-%m-%dT%H:%M:%S")
     t2 = datetime.strptime(t2.split("+")[0], "%Y-%m-%dT%H:%M:%S")  # 2020-02-08T14:40:00+01:00
     delta = t2 - t1
     return delta.seconds
+
+
+# {'passingTimes': [{'destination': {'fr': 'MOORTEBEEK', 'nl': 'MOORTEBEEK'}, 'expectedArrivalTime': '2020-02-10T17:36:00+01:00', 'lineId': '46'}
+def sort_and_truncate_to(number, passingTimes):
+    if len(passingTimes) == 0:
+        return passingTimes
+    elif len(passingTimes) > 1:
+        LOGGER.warn("Should have gotten only one element in list this can cause weird behavior: " + str(passingTimes))
+    time_getter = lambda j: datetime.strptime(j['expectedArrivalTime'].split("+")[0], "%Y-%m-%dT%H:%M:%S")
+    res = []
+    c = 0
+    for passing in passingTimes[0]['passingTimes']:
+        if "expectedArrivalTime" in passing.keys():
+            res.append(passing)
+            print(c, res, passing)
+            res.sort(key=(lambda p: time_getter(p)))
+            c += 1
+            if len(res) > number:
+                print("hey!")
+                res.pop()
+    res = {'passingTimes': res, 'pointId': passingTimes[0]['pointId']}
+    res = [res]
+    return res
 
 
 class Passages():
@@ -67,7 +90,7 @@ class Passages():
         self.shapefile_info = ShapefileReader(loop, session, client_id, client_secret)
         self._linesinfo = None
 
-    async def update_passages(self, now=None):
+    async def update_passages(self, now=None, max_passages_per_stop_id=5):
         if self._linesinfo is None:
             self._linesinfo = await self.shapefile_info.get_stop_info(self.stop_name, self.filtered_out_stop_ids)
         """Get passages info for given stopname."""
@@ -86,12 +109,13 @@ class Passages():
 
         common = CommonFunctions(self.loop, self.session, self.client_id, self.client_secret)
 
-        callURL = BASE_URL + PASSING_TIME_BY_POINT_SUFFIX + \
-                      str(stop_ids).replace("'", "")\
-                                   .replace(" ", "")\
-                                   .replace(",", "%2C")\
-                                   .replace("[", "")\
-                                   .replace("]", "")
+        if now is None:
+            now = datetime.now()
+            LOGGER.info("Fetching stop data at : " + str(now))
+
+        # todo hotfix while we get an answer on multiple ids in the same call
+        """
+        callURL = BASE_URL + PASSING_TIME_BY_POINT_SUFFIX + "%2C".join(stop_ids)
 
         print("Calling URL: " + callURL)
         raw_result = await common.api_call(callURL, {'Accept': 'application/json'})
@@ -100,6 +124,16 @@ class Passages():
             LOGGER.info(now)
         print("Got result from "+callURL+" : "+str(raw_result))
         json_result = json.loads(raw_result)
+        """
+        json_result = {'points': []}
+        for stop_id in stop_ids:
+            callURL = BASE_URL + PASSING_TIME_BY_POINT_SUFFIX + stop_id
+
+            print("Calling URL: " + callURL)
+            raw_result = await common.api_call(callURL, {'Accept': 'application/json'})
+            print("Got result from " + callURL + " : " + str(raw_result))
+            json_result['points'].extend(sort_and_truncate_to(2, json.loads(raw_result)['points']))
+        print(json_result)
 
         new_passages = []
         for point_info_json in json_result['points']:
@@ -107,13 +141,17 @@ class Passages():
             for pass_json in point_info_json['passingTimes']:
                 pass_dest = pass_json['destination'][self.lang]
                 pass_arrival_time = pass_json['expectedArrivalTime']
+                pass_msg = ""
+                if "message" in pass_json.keys():
+                    pass_msg = pass_json["message"][self.lang]
                 pass_arriving_in_seconds = get_time_delta(now, pass_arrival_time)
-                pass_arriving_in_minutes = pass_arriving_in_seconds//60
-                pass_arriving_in_seconds = pass_arriving_in_seconds - (pass_arriving_in_minutes*60)
+                pass_arriving_in_minutes = pass_arriving_in_seconds // 60
+                pass_arriving_in_seconds = pass_arriving_in_seconds - (pass_arriving_in_minutes * 60)
                 pass_line_number = pass_json['lineId']
                 passage = {"destination": pass_dest,
                            "arrival_time": pass_arrival_time,
                            "point_id": point_id,
+                           "message": pass_msg,
                            "arriving_in": {"min": pass_arriving_in_minutes, "sec": pass_arriving_in_seconds}}
                 additional_info = self._linesinfo[pass_line_number][0].copy()
                 additional_info.pop("stop_id")
