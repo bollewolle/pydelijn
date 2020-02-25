ba46966d2b02438e814b93c55af2a11bddcb80e4
Helldog136 <130792@student.umons.ac.be>
2020-02-26 00:26:22 +0100

    fixed negative values for time deltas

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pystibmvib",
-    version="0.0.5",
+    version="0.0.6",
     author="helldog136",
     author_email="dev.helldog136@outlook.com",
     python_requires=">=3.5.0",
@@ -15,7 +15,7 @@ setuptools.setup(
     long_description=LONG,
     long_description_content_type="text/markdown",
     url="https://github.com/helldog136/pystibmvib",
-    download_url="https://github.com/helldog136/pystibmvib/archive/0.0.5.tar.gz",
+    download_url="https://github.com/helldog136/pystibmvib/archive/0.0.6.tar.gz",
     packages=setuptools.find_packages(exclude=('tests',)),
     install_requires=[
         'async_timeout',
