8e8a8a3d82a840225f93db4ae973f1025583b49c
Helldog136 <130792@student.umons.ac.be>
2020-02-27 10:58:18 +0100

    fixed arriving in minutes jumping to 1439 when the arrival datetime is in the past. Now stays at 0

--- pystibmvib/api.py
+++ pystibmvib/api.py
@@ -8,6 +8,7 @@ This code is released under the terms of the MIT license. See the LICENSE
 file for more details.
 """
 import json
+import math
 from datetime import *
 
 import pytz
@@ -36,8 +37,8 @@ def get_time_delta(t1, t2):
     if not type(t1) == type(datetime.now()):
         t1 = datetime.strptime(str(t1), "%Y-%m-%dT%H:%M:%S")
     t2 = datetime.strptime(t2.split("+")[0], "%Y-%m-%dT%H:%M:%S")  # 2020-02-08T14:40:00+01:00
-    delta = t2 - t1
-    ret = max(0, delta.seconds)
+    delta = t2.timestamp() - t1.timestamp()
+    ret = max(0, int(math.floor(delta)))
     return ret
 
 
